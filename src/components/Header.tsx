eb2e0e14927199ad09d588bff6e8b9faed86894a
Sergey Kabanets <skabanets97@gmail.com>
2025-03-12 14:11:06 +0200

    Update Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo, UnreadMessagesCounter } from '../components';
 
 export const Header = () => {
   return (
-    <div className="px-2.5 py-2 h-[54px] bg-gray-800 flex justify-between items-center">
+    <div className="px-4 py-2 h-[54px] bg-gray-800 flex justify-between items-center">
       <Logo />
       <UnreadMessagesCounter />
     </div>
